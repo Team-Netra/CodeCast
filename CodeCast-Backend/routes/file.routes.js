8270300a75d4de3a476f9251e16c2dceb27b83b6
PrachiJha-404 <prachijha1805@gmail.com>
2026-01-06 00:34:03 +0530

    feat(routes): Add file routing

---
+++ CodeCast-Backend/routes/file.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { 
+    saveFileContents, 
+    getRoomFiles, 
+    getFileContents,
+    createFile,
+    deleteFile
+} from "../controllers/file.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+// All routes require authentication
+router.use(verifyJWT);
+
+// File operations
+router.route("/save").patch(saveFileContents);
+router.route("/create").post(createFile);
+router.route("/:fileId").get(getFileContents).delete(deleteFile);
+
+// Room files
+router.route("/room/:roomId").get(getRoomFiles);
+
+export default router;
